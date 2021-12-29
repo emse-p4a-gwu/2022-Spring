73fc97e0efca4bf97fcdcb356e7767957f7e095a
John Helveston <john.helveston@gmail.com>
2021-12-29 14:46:06 -0500

    rebuilt with new child docs

--- settings.R
+++
@@ -1,6 +0,0 @@
-class_repo <- "https://github.com/emse-p4a-gwu/2020-Fall"
-class_title <- 'EMSE 4571: Intro to Programming for Analytics'
-class_weekday <- "Thursdays"
-class_time <- "12:45 - 3:15 PM"
-class_room <- 'Tompkins 208'
-class_room_url <- "https://acadtech.gwu.edu/location?staging&id=351&selected=Tompkins&method=building"
