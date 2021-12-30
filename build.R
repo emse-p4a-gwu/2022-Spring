2683356e9fc5472577cb9268f0855e42947cfba4
John Helveston <john.helveston@gmail.com>
2021-12-30 10:27:00 -0500

    more site reorganization

--- build.R
+++ build.R
@@ -1,2 +1,8 @@
+# Render whole site
 rmarkdown::render_site()
+
+# Render README file
 rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
+
+# Render footer
+source(file.path('child', 'footer.R'))
