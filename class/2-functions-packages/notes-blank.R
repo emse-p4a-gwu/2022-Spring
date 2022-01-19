90dced111267a99b752eef2cc27cf2c08c7275fc
jhelvy <john.helveston@gmail.com>
2022-01-19 15:26:32 +0000

    Deploying to gh-pages from  @ fa3ab96cc51ae8efbb508f5bcd622b4db3867ebf 🚀

--- class/2-functions-packages/notes-blank.R
+++ class/2-functions-packages/notes-blank.R
@@ -65,9 +65,14 @@ false     <- as.logical(as.numeric(TRUE))
 
 # Polya practice 1: What's your degree worth?
 
-# In the U.S., the average annual salary of a high school graduate is $35,256, and the average salary of a GW graduate is $76,151. However, GW grads pay an average of $70,000 / year (tuition + fees + housing) for 4 years for their degree, and high school grads are working that entire time. 
+# In the U.S., the average annual salary of a high school graduate is $35,256,
+# and the average salary of a GW graduate is $76,151. However, GW grads pay an
+# average of $70,000 / year (tuition + fees + housing) for 4 years for their
+# degree, and high school grads are working that entire time.
 
-# Assuming immediate employment after graduation, how many years after graduating will the GW grad need to work until their net income (salary minus cost of education) surpasses that of the average high school graduate?
+# Assuming immediate employment after graduation, how many years after
+# graduating will the GW grad need to work until their net income (salary minus
+# cost of education) surpasses that of the average high school graduate?
 
 
 
@@ -84,9 +89,14 @@ false     <- as.logical(as.numeric(TRUE))
 
 # Polya practice 2: Prius vs. Camry
 
-# Kevin is deciding between purchasing a Toyota Prius, which sells for $27,600, and a Toyota Camry, which sells for $24,000. He knows he can get an average fuel economy of 55 miles per gallon (mpg) in the Prius but only 28 mpg in the Camry on average. He also knows that he typically drives 12,000 miles each year, and the average price of gasoline is $3.00 / gallon.
+# Kevin is deciding between purchasing a Toyota Prius, which sells for $27,600,
+# and a Toyota Camry, which sells for $24,000. He knows he can get an average
+# fuel economy of 55 miles per gallon (mpg) in the Prius but only 28 mpg in the
+# Camry on average. He also knows that he typically drives 12,000 miles each
+# year, and the average price of gasoline is $3.00 / gallon.
 
-# How long (in years) would Kevin have to drive the Prius for the money he saves in fuel savings to be greater than the price premium compared to the Camry?
+# How long (in years) would Kevin have to drive the Prius for the money he saves
+# in fuel savings to be greater than the price premium compared to the Camry?
 
 
 
