d22d7f02961e57e135ce5724c04bab40b491339e
jhelvy <john.helveston@gmail.com>
2022-02-11 14:38:35 +0000

    Deploying to gh-pages from  @ 1819ec0f47925d0950c611f1dbc22967ab6fa174 🚀

--- child/settings.R
+++ child/settings.R
@@ -6,7 +6,7 @@ settings <- list(
     time     = "12:45 - 3:15 PM EST",
     room     = 'Tompkins 208',
     room_url = "https://acadtech.gwu.edu/location?staging&id=351&selected=Tompkins&method=building",
-    schedule_url = 'https://docs.google.com/spreadsheets/d/1-qdmyYw4kkECodszkWRqVH08LeFRnu2u99X4_d5UtH0/edit?usp=sharing',
+    schedule_url = 'https://docs.google.com/spreadsheets/d/1GrDHCSKoFyOESO-lqf0ChIRL25R1A9eoMYD5OOuMNLg/edit?usp=sharing',
     dates = "Jan. 13 - Apr. 28, 2022",
     slack = "https://emse-p4a-s22.slack.com"
 )
